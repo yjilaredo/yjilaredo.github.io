e90502e46dd084f93c40a9cf0a2f9573a6d6a084
yjilaredo <offlimitzoff@gmail.com>
2026-07-14 18:07:53 +0800

    Update website

--- script.js
+++ script.js
@@ -97,6 +97,8 @@ hamburger.addEventListener("click", () => {
 
     navLinks.classList.toggle("active");
 
+    hamburger.classList.toggle("active");
+
 });
 
 // Close menu after clicking a link
@@ -106,6 +108,7 @@ document.querySelectorAll("#nav-links a").forEach(link => {
     link.addEventListener("click", () => {
 
         navLinks.classList.remove("active");
+hamburger.classList.remove("active");s
 
     });
 
